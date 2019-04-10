c893b4c59662b84a9be9f6a391eaf502acdb7977
2015136011 <43721188+2015136011@users.noreply.github.com>
2019-04-10 22:36:07 +0900

    2015136011

--- PHP-Programming-master/Experiments/TableBoard_Shop/index.php
+++ PHP-Programming-master/Experiments/TableBoard_Shop/index.php
@@ -15,6 +15,7 @@
 <head>
     <title>Table V01</title>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!--===============================================================================================-->
     <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
